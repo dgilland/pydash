34fcaf64ea386b6c1b2211637ec103d08e25fe0a
Derrick Gilland <dgilland@gmail.com>
2013-05-29 23:43:37 -0400

    Add functions: head, flatten, index_of, and initial.

--- pydash/__init__.py
+++ pydash/__init__.py
@@ -1,3 +1,4 @@
+import collections
 
 def compact(array):
     '''
@@ -5,7 +6,7 @@ def compact(array):
 
     Creates a list with all falsey values of array removed.
 
-    :param array: list to compact
+    :param list array: list to compact
     :rtype: list
     '''
     return filter(None, array)
@@ -16,15 +17,15 @@ def difference(array, *lists):
 
     Creates a list of list elements not present in the other lists
 
-    :param array: the list to process
-    :param lists: lists to check
+    :param list array: the list to process
+    :param list lists: lists to check
     :rtype: list
     '''
     return list( difference(set(array).difference(lists[0]), *lists[1:]) ) if lists else array
 
 def rest(array, callback=None):
     '''
-    .. py:method:: rest(array[, callback|n|pluck|where])
+    .. py:method:: rest(array[, callback|n|pluck|where=None])
 
     Return all but the first value of array.
     If a number n is passed, the first n values are excluded from the result.
@@ -33,7 +34,7 @@ def rest(array, callback=None):
     If a property name is passed for callback, the created "_.pluck" style callback will return the property value of the given element.
     If an object is passed for callback, the created "_.where" style callback will return true for elements that have the properties of the given object, else false.
 
-    :param array: the list to process
+    :param list array: the list to process
     :param mixed callback: callback to filter by
     :rtype: list
     '''
@@ -53,8 +54,8 @@ def find_index(array, callback):
 
     This method is similar to _.find, except that it returns the index of the element that passes the callback check, instead of the element itself.
 
-    :param array: list to process
-    :param callback: filter function or where dict
+    :param list array: list to process
+    :param function callback: filter function or where dict
     :rtype: list
     '''
     n = -1
@@ -67,7 +68,7 @@ def find_index(array, callback):
 
 def first(array, callback=None):
     '''
-    .. py:method:; first(array[, callback|n|pluck|where)
+    .. py:method:: first(array[, callback|n|pluck|where=None])
 
     Gets the first element of the array.
     If a number n is passed, the first n elements of the array are returned.
@@ -75,6 +76,10 @@ def first(array, callback=None):
     The callback is invoked with three arguments; (value, index, array).
     If a property name is passed for callback, the created "_.pluck" style callback will return the property value of the given element.
     If an object is passed for callback, the created "_.where" style callback will return true for elements that have the properties of the given object, else false.
+
+    :param list array: list to select from
+    :param mixed callback: callback to filter array
+    :rtype: mixed
     '''
     n = 0
     for is_true, _, _, _ in _iter_callback(array, callback):
@@ -87,6 +92,77 @@ def first(array, callback=None):
 
     if callback in (None, 1):
         ret = ret[0]
+
+    return ret
+
+head = first
+
+def flatten(array, callback=None, _depth=0):
+    '''
+    .. py:method:: flatten(array[, callback|pluck|where=None])
+
+    Flattens a nested array (the nesting can be to any depth).
+    If callback is True, array will only be flattened a single level.
+    If callback is passed, each element of array is passed through a callback before flattening.
+
+    :param list array: list to flatten
+    :param mixed callback: callback to filter array
+    :rtype: list
+    '''
+
+    shallow = False
+    if callback is True:
+        shallow = True
+    elif callback:
+        array = map(_make_callback(callback), array)
+        callback = None
+
+    lst = []
+    if all([ isinstance(array, collections.Iterable), not isinstance(array, basestring), not (shallow and _depth > 1) ]):
+        for a in array:
+            lst.extend(flatten(a, callback, _depth+1))
+    else:
+        lst.append(array)
+
+    return lst
+
+def index_of(array, value, from_index=0):
+    '''
+    .. py:method:: index_of(array, value[, from_index=0])
+
+    Gets the index at which the first occurrence of value is found
+
+    :param list array: list to search
+    :param mixed value: value to search for
+    :param integer from_index: the index to search from
+    :rtype: integer
+    '''
+    try:
+        return array.index(value, from_index)
+    except ValueError:
+        return False
+
+def initial(array, callback=1):
+    '''
+    .. py:method:: initial(array[, callback|n|pluck|where=None)
+
+    Gets all but the last element of array.
+    If a number n is passed, the last n elements are excluded from the result.
+    If a callback function is passed, elements at the end of the array are excluded from the result as long as the callback returns truthy.
+
+    :param list array: list to query
+    :param mixed callback: The function called per element or the number of elements to exclude
+    '''
+
+    lst = array[::-1]
+    n = len(array)
+    for is_true, _, _, _ in _iter_callback(lst, callback):
+        if is_true:
+            n -= 1
+        else:
+            break
+
+    ret = array[:n]
 
     return ret
 
@@ -119,10 +195,11 @@ def pluck(collection, key):
     '''
     return map(lambda x: x.get(key), collection)
 
-def _iter_callback(array, callback=None):
+def _make_callback(callback):
+    '''Create a callback function from a mixed type `callback`'''
     if hasattr(callback, '__call__'):
         cb = callback
-    elif isinstance(callback, (str,unicode)):
+    elif isinstance(callback, basestring):
         key = callback
         cb = lambda item, *args: pluck([item], key)[0]
     elif isinstance(callback, dict):
@@ -131,4 +208,9 @@ def _iter_callback(array, callback=None):
         index = callback if isinstance(callback, int) else 1
         cb = lambda item, i, *args: i < index
 
+    return cb
+
+def _iter_callback(array, callback=None):
+    cb = _make_callback(callback)
     return ( (cb(item, i, array), item, i, array) for i, item in enumerate(array) )
+
--- tests/test_pydash.py
+++ tests/test_pydash.py
@@ -97,3 +97,46 @@ def test_first():
     # using "_.where" callback shorthand
     assert _.first(food, { 'type': 'fruit' }) == [{ 'name': 'apple', 'type': 'fruit' }, { 'name': 'banana', 'type': 'fruit' }]
 
+    # verify alias
+    _.head is _.first
+
+def test_flatten():
+    array = [1, ['2222'], [3, [[4]]]]
+    assert _.flatten(array) == [1, '2222', 3, 4]
+    assert _.flatten(array, True) == [1, '2222', 3, [[4]]]
+
+    # test pluck style callback
+    stooges = [
+        { 'name': 'curly', 'quotes': ['Oh, a wise guy, eh?', 'Poifect!'] },
+        { 'name': 'moe', 'quotes': ['Spread out!', 'You knucklehead!'] }
+    ]
+
+    assert _.flatten(stooges, 'quotes') == ['Oh, a wise guy, eh?', 'Poifect!', 'Spread out!', 'You knucklehead!']
+
+def test_index_of():
+    assert _.index_of([1, 2, 3, 1, 2, 3], 2) == 1
+    assert _.index_of([1, 2, 3, 1, 2, 3], 2, 3) == 4
+    assert _.index_of([1, 1, 2, 2, 3, 3], 2, True) == 2
+    assert _.index_of([1, 1, 2, 2, 3, 3], 4) is False
+    assert _.index_of([1, 1, 2, 2, 3, 3], 2, 10) is False
+
+def test_initial():
+    assert _.initial([1, 2, 3]) == [1, 2]
+    assert _.initial([1, 2, 3], 2) == [1]
+    assert _.initial([1, 2, 3], lambda num, *args: num > 1) == [1]
+
+    # test pluck style callback
+    food = [
+        { 'name': 'beet',   'organic': False },
+        { 'name': 'carrot', 'organic': True }
+    ]
+    assert _.initial(food, 'organic') == [{ 'name': 'beet',   'organic': False }]
+
+    # test where style callback
+    food = [
+        { 'name': 'banana', 'type': 'fruit' },
+        { 'name': 'beet',   'type': 'vegetable' },
+        { 'name': 'carrot', 'type': 'vegetable' }
+    ]
+    assert _.initial(food, { 'type': 'vegetable' }) == [{ 'name': 'banana', 'type': 'fruit' }]
+
