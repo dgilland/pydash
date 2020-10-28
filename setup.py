d4aad9aedb555941c72fba093330ea2d33ce2187
Derrick Gilland <dgilland@gmail.com>
2020-10-28 18:28:24 -0400

    Move package_dir, packages, and include_package_data options from setup.py to setup.cfg.

--- setup.py
+++ setup.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
-package_dir = 'src'
-packages = find_packages(package_dir)
-
-
-setup(
-    package_dir={'': package_dir},
-    packages=packages,
-    include_package_data=True
-)
+setup()
