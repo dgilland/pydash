2699515b2cc53bf49bdec47fd1efe9a1941578c6
Derrick Gilland <dgilland@gmail.com>
2020-10-28 18:27:49 -0400

    Set package version in setup.cfg instead of __version__.py.

--- setup.py
+++ setup.py
@@ -7,13 +7,8 @@ from setuptools import setup, find_packages
 package_dir = 'src'
 packages = find_packages(package_dir)
 
-version = {}
-with open('{}/{}/__version__.py'.format(package_dir, packages[0])) as f:
-    exec(f.read(), version)
-
 
 setup(
-    version=version['__version__'],
     package_dir={'': package_dir},
     packages=packages,
     include_package_data=True
--- src/pydash/__version__.py
+++ src/pydash/__version__.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
-"""Project version information.
-"""
+"""Project version information."""
 
-__version__ = '4.9.0'
+from pkg_resources import DistributionNotFound, get_distribution
+
+
+try:
+    __version__ = get_distribution(__name__.split(".")[0]).version
+except DistributionNotFound:  # pragma: no cover
+    # Package is not installed.
+    __version__ = None
